dd750829abc3c0a437979b456d656e5a9025ee14
minhtri228 <63711185+minhtri228@users.noreply.github.com>
2022-10-07 21:34:35 +0700

    second

---
+++ models/User.js
@@ -0,0 +1,10 @@
+class User{
+    constructor(id,gmail ,name ,sdt ,content ){
+        this.id=id,
+        this.gmail=gmail,
+        this.name=name,
+        this.sdt=sdt,
+        this.content=content
+    }
+}
+module.exports= User;
--- src/app.js
+++ src/app.js
@@ -2,45 +2,90 @@ const express = require("express");
 const app = express();
 const path =require("path");
 var mysql = require('mysql');
+//import { SOME_OBJECT } from './models';
 // const { dirname } = require("path");
 var db = mysql.createPool({
-    host: 'localhost',
+    host:'127.0.0.1:3300',
     user:'root',
-    password: 'admin',
-    database:'gtbt'
+    password:'admin',
+    database:'gtbt',
 });
-const port = process.env.port || 3000;
+
+// let connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'admin',
+//     database: 'gtbt'
+// });
+
+app.get("/connection",(req,res)=>{
+    db.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected!!!")
+      });
+});
+
+const port = process.env.port || 3001;
+
 app.set("view engine","ejs");
-app.set("views",path.join(__dirname,'../public'));
+app.set("views",path.join(__dirname,'./views'));
 app.get("/",(req,res)=>{
     res.render("home");
 });
-const static_path = path.join( __dirname , " ../public");
-console.log(path.join(__dirname,'../public'));
+const static_path = path.join(__dirname,"../src");
+console.log(static_path);
 app.use(express.static(static_path));
+app.get('/facbook',function(req,resp){
+    resp.redirect("https://www.facebook.com/tri.paipi/");
+});
+app.get('/github',function(req,resp){
+    resp.redirect("https://github.com/minhtri228/lab_PTUD.git");
+});
+app.get('/instagram',function(req,resp){
+    resp.redirect("https://www.instagram.com/trizuize/");
+});
+app.get("/all", function(req,resp){
+    const sql = `select * from user`;
+    console.log(sql);
+    // db.query(sql, function(err, results) {
+        // if (err) throw err;
+        // console.log(results);
+        // res.redirect("/");
+    //})
+});
 
 app.get('/testconnect',function(req,res,next){
     if(db!=null){
+        console.log(db);
         res.send("connect successful");
     } else{
         res.send('connect fail');
     }
-  });
+});
   
 app.get('/form',function(req,res,next){
     res.send('form');
 });
 
-app.post('/insert', function(req, res, next) {
-    var name = req.body.name;
-    var content = req.body.content;
-    var sdt = req.body.sdt;
-    var sql = `INSERT INTO user (id, insert, sdt, content) VALUES ("${name}", "${sdt}", "${content}", NOW())`;
-    db.query(sql,function(){
-      res.redirect('/');
-    });
-  });
 
+app.post('/insert', function(req, res, next) {
+    //const id =req.query.email;
+    const email =req.query.email;
+    const name = req.query.name;
+    const content = req.query.content;
+    const sdt = req.query.sdt;
+    // console.log(req.query.email+" ");
+    // console.log(email+" "+name+" "+content+" "+sdt+" ");
+    console.log(db);
+    const sql = `INSERT INTO user (email,name, sdt, content) VALUES ("${email}","${name}", "${sdt}", "${content}")`;
+    console.log(sql);
+    db.query(sql, function(err, results) {
+        if (err) throw err;
+        console.log(results);
+        res.redirect("/");
+    })
+});
+module.exports = app;
 app.listen(port,()=>{
     console.log(`server is running at port no ${port}`);
 });
